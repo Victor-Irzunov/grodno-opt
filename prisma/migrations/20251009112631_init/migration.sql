b690fa4a5dc85a5c8f9f69324a365779c4a35c8d
Виктор <Irzunovv@gmail.com>
2025-10-09 14:27:01 +0300

    -2

--- prisma/migrations/20251009112631_init/migration.sql
+++ prisma/migrations/20251009112631_init/migration.sql
@@ -13,9 +13,9 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `UserData` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `fullName` VARCHAR(191) NOT NULL,
-    `address` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL DEFAULT '',
+    `address` VARCHAR(191) NOT NULL DEFAULT '',
+    `phone` VARCHAR(191) NOT NULL DEFAULT '',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `userId` INTEGER NOT NULL,
@@ -163,6 +163,19 @@ CREATE TABLE `ReturnItem` (
     `orderItemId` INTEGER NOT NULL,
     `quantity` INTEGER NOT NULL,
     `refundAmount` DECIMAL(12, 2) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ContactRequest` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `company` VARCHAR(191) NULL,
+    `message` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `isViewed` BOOLEAN NOT NULL DEFAULT false,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
